4dd572309d0666d1d40ea0355b2d93d8898bf6c3
Pablo Moraes De Mattos <pablomoraesdemattos@gmail.com>
2023-10-15 12:01:01 -0300

    Desafio calculadora de partidas

---
+++ funcaoCalculaPartidasRankeadas.js
@@ -0,0 +1,29 @@
+let saldoVitorias = calculadoraPartidasRankeadas(86,5)
+let nivel = classificaNivelHeroi(saldoVitorias)
+
+function calculadoraPartidasRankeadas(vitorias,derrotas){
+    let saldo = vitorias - derrotas
+    return saldo
+}
+function classificaNivelHeroi(saldo){
+    let nivelHeroi
+    if(saldoVitorias <= 10){
+        nivelHeroi = "Ferro"
+    }else if(saldoVitorias <= 20){
+        nivelHeroi = "Bronze"
+    }else if(saldoVitorias <= 50){
+        nivelHeroi = "Prata"
+    }else if(saldoVitorias <= 80){
+        nivelHeroi = "Ouro"
+    }else if(saldoVitorias <= 90){
+        nivelHeroi = "Diamante"
+    }else if(saldoVitorias <= 100){
+        nivelHeroi = "Lendário"
+    }else if(saldoVitorias >= 101){
+        nivelHeroi = "Imortal"
+    }
+    return nivelHeroi
+}
+    
+
+console.log("O herói tem saldo de "+saldoVitorias+" vitórias e está no nível "+nivel)
